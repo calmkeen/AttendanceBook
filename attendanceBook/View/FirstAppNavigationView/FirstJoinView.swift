95235513c37bf5ee38c2989a8bc8129952a5c8c6
calmkeen <iyunseong159@gmail.com>
2023-04-08 17:36:13 +0900

    #1 attaendanceBook

---
+++ attendanceBook/View/FirstAppNavigationView/FirstJoinView.swift
@@ -0,0 +1,69 @@
+//
+//  FirstJoinView.swift
+//  attendanceBook
+//
+//  Created by calmkeen on 2023/04/08.
+//
+/*
+ 처음 앱을 설치한 사람에게 뜨는 뷰로써 간단한 가이드를 제공합니다.
+ 메인기능은 group과 각 탭이 무엇을 설명해주는 지 설명해주면서 동시에
+ 안내적인 기능을 제공합니다.
+ 앱을 처음 켤 시에 한번만 나오게 세팅되어야 합니다.
+ */
+
+import UIKit
+import SnapKit
+
+class FirstJoinView: UIViewController{
+    
+    var backgroundImage: UIImageView = {
+        let image = UIImageView()
+        return image
+    }()
+    
+    var naviLabel: UILabel = {
+        let label = UILabel()
+        label.text = "제대로 관리해 보자구!"
+        return label
+    }()
+    
+    var pointImage: UIImageView = {
+        let image = UIImageView()
+        return image
+    }()
+    var nextBtn: UIButton = {
+        let btn = UIButton()
+    }
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        makeView()
+        makeConstraints()
+    }
+    func makeView() {
+        self.view.addSubview(backgroundImage)
+        self.view.addSubview(naviLabel)
+        self.view.addSubview(pointImage)
+    }
+    
+    func makeConstraints() {
+        backgroundImage.snp.makeConstraints{ make in
+            make.edges.equalToSuperview()
+        }
+        naviLabel.snp.makeConstraints{ make in
+            make.top.equalTo(backgroundImage.snp.top).offset(30)
+        }
+        pointImage.snp.makeConstraints{ make in
+            make.bottom.equalTo(backgroundImage.snp.bottom).offset(30)
+        }
+        nextBtn.snp.makeConstraints{ make in
+            make.top.equalTo(backgroundImage.snp.top)
+            make.trailing.equalTo(backgroundImage.snp.trailing).offset(5)
+        }
+    }
+    func nextBtnEvent() {
+//        if nextBtn == .toggle {
+//            //명확하지 않지만 명시적으로 다음뷰를 호출하는 느낌으로 이렇게 적었습니다. 네비게이션으로 변경할거기 떄문에 지그음 대충 하겠습니다.
+//            append(LoginNavigationView())
+//        }
+    }
+}
---
+++ attendanceBook/View/FirstAppNavigationView/LoginNavigationView.swift
@@ -0,0 +1,72 @@
+//
+//  LoginNavigationView.swift
+//  attendanceBook
+//
+//  Created by calmkeen on 2023/04/08.
+//
+
+
+/*
+로그인을 유도하면서 로그인없이 앱에 접근할수 있게 해주는 마지막 뷰입니다.
+ 가운데를 로티 label로 작성한 이유는
+MainFunc 변경을 고려하여 나중에 추가될 메인기능을
+ 작성하기 위해 로티 및 라벨로 네이밍 해 두었습니다.
+ 
+ */
+
+import UIKit
+import SnapKit
+
+
+class LoginNavigationView: UIViewController {
+    
+    var backgroundLottie: UIImageView = {
+        let background = UIImageView()
+        return background
+    }()
+    var labelList: UIView = {
+        let list = UIView()
+        return list
+        
+    }()
+    var loginNaviBtn: UIButton = {
+        let btn = UIButton()
+//        btn.titleLabel = "로그인"
+        return btn
+    }()
+    var skipBtn: UIButton = {
+        let btn = UIButton()
+//        btn.titleLabel = "skip"
+        return btn
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        makeView()
+        makeConstraint()
+    }
+    
+    func makeView() {
+        self.view.addSubview(backgroundLottie)
+        self.view.addSubview(labelList)
+        self.view.addSubview(loginNaviBtn)
+        self.view.addSubview(skipBtn)
+    }
+    
+    func makeConstraint() {
+        backgroundLottie.snp.makeConstraints{ make in
+            make.edges.equalToSuperview()
+        }
+        labelList.snp.makeConstraints{ make in
+            make.top.equalTo(backgroundLottie.snp.top).offset(50)
+        }
+        loginNaviBtn.snp.makeConstraints{ make in
+            make.bottom.equalTo(backgroundLottie.snp.bottom)
+            make.leading.equalTo(backgroundLottie.snp.leading).offset(5)
+        }
+        skipBtn.snp.makeConstraints{ make in
+            make.bottom.equalTo(backgroundLottie.snp.bottom)
+            make.trailing.equalTo(backgroundLottie.snp.trailing).offset(5)
+        }
+    }
+}
